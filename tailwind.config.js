68ed37e4b5d483e8a78144fe53f3ff23445de8ee
Nguyen Thanh Nghi <thanhnghi1421@gmail.com>
2023-05-24 00:56:03 +0700

    add button, input components

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ export default {
     './resources/**/*.vue',
  ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': {
+        400: '#34d399',
+        DEFAULT: '#15803d',
+        700: '#047857',
+      },
+      'secondary': '#fb923c',
+      'error': '#dc2626',
+      'danger': '#fbbf24',
+      'success': '#22c55e'
+    },
+    
   },
   plugins: [],
 }
